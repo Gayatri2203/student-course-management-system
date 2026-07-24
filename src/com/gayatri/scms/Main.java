dcd3bc983121e45470e9a1726194ce42f57e3e28
Gayatri <gayatripotdar0@gmail.com>
2026-07-24 16:15:54 +0530

    Refactor code using methods

--- src/com/gayatri/scms/Main.java
+++ src/com/gayatri/scms/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("===== Student Course Management System =====");
+        displayWelcomeMessage();
 
         System.out.print("Enter Student Name: ");
         String name = scanner.nextLine();
@@ -15,12 +16,22 @@ public class Main {
         System.out.print("Enter Student Age: ");
         int age = scanner.nextInt();
 
+        displayStudentDetails(name, age);
+
+        scanner.close();
+    }
+
+    public static void displayWelcomeMessage() {
+        System.out.println("====================================");
+        System.out.println(" Student Course Management System");
+        System.out.println("====================================");
+    }
+
+    public static void displayStudentDetails(String name, int age) {
         System.out.println();
         System.out.println("Student Details");
         System.out.println("----------------");
         System.out.println("Name : " + name);
         System.out.println("Age  : " + age);
-
-        scanner.close();
     }
 }
